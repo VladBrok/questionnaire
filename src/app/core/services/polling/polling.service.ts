ad964f1292e64dc1f7d6eb0ea63b86181bb7e712
VladBrok <vlad.brok@gmail.com>
2023-10-26 17:22:44 +0300

    refactor polling

--- src/app/core/services/polling/polling.service.ts
+++ src/app/core/services/polling/polling.service.ts
@@ -1,86 +1,62 @@
 import { Injectable } from '@angular/core';
 import {
-  BehaviorSubject,
   Observable,
-  Subscriber,
-  Subscription,
+  Subject,
+  filter,
   fromEvent,
+  merge,
+  repeat,
+  switchMap,
+  takeUntil,
+  tap,
   timer,
 } from 'rxjs';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 const POLL_INTERVAL_MS = 10000;
 @Injectable({
   providedIn: 'root',
 })
 export class PollingService {
-  private isTabActive = true;
-  private isOnline = true;
-  readonly tabActiveAndOnline = new BehaviorSubject(true);
-
-  constructor() {
-    fromEvent(document, 'visibilitychange')
-      .pipe(takeUntilDestroyed())
-      .subscribe(() => {
-        this.isTabActive = !document.hidden;
-        this.update();
-      });
-    fromEvent(window, 'online')
-      .pipe(takeUntilDestroyed())
-      .subscribe(() => {
-        this.isOnline = true;
-        this.update();
-      });
-    fromEvent(window, 'offline')
-      .pipe(takeUntilDestroyed())
-      .subscribe(() => {
-        this.isOnline = false;
-        this.update();
-      });
-  }
-
-  private update() {
-    this.tabActiveAndOnline.next(this.isTabActive && this.isOnline);
-  }
-
   startPolling<T>(getObservable: () => Observable<T>): Observable<T> {
-    return new Observable<T>(this.subscribe.bind(this, getObservable));
-  }
-
-  private subscribe<T>(getObservable: () => Observable<T>, sub: Subscriber<T>) {
-    const subscription = new Subscription();
-    let timerSubscription: Subscription | null = null;
-    let lastPoll: Date | null = null;
-
-    const checkLastPoll = () => {
-      if (
-        lastPoll &&
-        new Date().getTime() - lastPoll.getTime() > POLL_INTERVAL_MS
-      ) {
-        timerSubscription?.unsubscribe();
-        timerSubscription = timer(0, POLL_INTERVAL_MS).subscribe(poll);
-      }
-    };
-    const poll = () => {
-      if (!this.tabActiveAndOnline.value) return;
-      subscription.add(
-        getObservable().subscribe({
-          next: sub.next.bind(sub),
-          error: sub.error.bind(sub),
-        })
-      );
-      lastPoll = new Date();
-    };
-    const teardown = () => {
-      subscription.unsubscribe();
-      timerSubscription?.unsubscribe();
-    };
-
-    subscription.add(
-      this.tabActiveAndOnline.subscribe((val) => val && checkLastPoll())
+    const start = new Subject<void>();
+    const stop = new Subject<void>();
+    let isStopped = false;
+    return merge(
+      timer(0, POLL_INTERVAL_MS).pipe(
+        takeUntil(stop),
+        repeat({ delay: () => start })
+      ),
+      fromEvent(document, 'visibilitychange').pipe(
+        filter(() => !document.hidden)
+      ),
+      fromEvent(window, 'online'),
+      start.pipe(
+        tap(() => (isStopped = false)),
+        filter(() => false)
+      ),
+      stop.pipe(
+        tap(() => (isStopped = true)),
+        filter(() => false)
+      )
+    ).pipe(
+      tap(
+        (e) =>
+          (!navigator.onLine || document.hidden) &&
+          typeof e === 'number' &&
+          stop.next()
+      ),
+      tap(
+        (e) =>
+          typeof e !== 'number' &&
+          navigator.onLine &&
+          !document.hidden &&
+          isStopped &&
+          start.next()
+      ),
+      filter(
+        (e) => navigator.onLine && !document.hidden && typeof e === 'number'
+      ),
+      switchMap(() => getObservable())
     );
-    timerSubscription = timer(0, POLL_INTERVAL_MS).subscribe(poll);
-
-    return teardown;
   }
 }
