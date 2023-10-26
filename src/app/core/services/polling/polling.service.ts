d7b8cc82e3e662d2f71ca1a3fcdbf95d32d17daa
VladBrok <vlad.brok@gmail.com>
2023-10-26 09:12:29 +0300

    extract subscribe method

--- src/app/core/services/polling/polling.service.ts
+++ src/app/core/services/polling/polling.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subscription, fromEvent, timer } from 'rxjs';
+import { Observable, Subscriber, Subscription, fromEvent, timer } from 'rxjs';
 
 const POLL_INTERVAL_MS = 10000;
 @Injectable({
@@ -7,55 +7,57 @@ const POLL_INTERVAL_MS = 10000;
 })
 export class PollingService {
   startPolling<T>(getObservable: () => Observable<T>): Observable<T> {
-    return new Observable((sub) => {
-      const subscription = new Subscription();
-      let timerSubscription: Subscription | null = null;
-      let isOnline = true;
-      let isTabActive = true;
-      let lastPoll: Date | null = null;
+    return new Observable<T>(this.subscribe.bind(this, getObservable));
+  }
 
-      const checkLastPoll = () => {
-        if (
-          lastPoll &&
-          new Date().getTime() - lastPoll.getTime() > POLL_INTERVAL_MS
-        ) {
-          timerSubscription?.unsubscribe();
-          timerSubscription = timer(0, POLL_INTERVAL_MS).subscribe(poll);
-        }
-      };
-      const handleVisibleChange = () => {
-        isTabActive = !document.hidden;
-        if (isTabActive) checkLastPoll();
-      };
-      const handleOnline = () => {
-        isOnline = true;
-        checkLastPoll();
-      };
-      const handleOffline = () => {
-        isOnline = false;
-      };
-      const poll = () => {
-        if (!isTabActive || !isOnline) return;
-        subscription.add(
-          getObservable().subscribe({
-            next: sub.next.bind(sub),
-            error: sub.error.bind(sub),
-          })
-        );
-        lastPoll = new Date();
-      };
+  private subscribe<T>(getObservable: () => Observable<T>, sub: Subscriber<T>) {
+    const subscription = new Subscription();
+    let timerSubscription: Subscription | null = null;
+    let isOnline = true;
+    let isTabActive = true;
+    let lastPoll: Date | null = null;
 
-      subscription.add(
-        fromEvent(document, 'visibilitychange').subscribe(handleVisibleChange)
-      );
-      subscription.add(fromEvent(window, 'online').subscribe(handleOnline));
-      subscription.add(fromEvent(window, 'offline').subscribe(handleOffline));
-      timerSubscription = timer(0, POLL_INTERVAL_MS).subscribe(poll);
-
-      return () => {
-        subscription.unsubscribe();
+    const checkLastPoll = () => {
+      if (
+        lastPoll &&
+        new Date().getTime() - lastPoll.getTime() > POLL_INTERVAL_MS
+      ) {
         timerSubscription?.unsubscribe();
-      };
-    });
+        timerSubscription = timer(0, POLL_INTERVAL_MS).subscribe(poll);
+      }
+    };
+    const handleVisibleChange = () => {
+      isTabActive = !document.hidden;
+      if (isTabActive) checkLastPoll();
+    };
+    const handleOnline = () => {
+      isOnline = true;
+      checkLastPoll();
+    };
+    const handleOffline = () => {
+      isOnline = false;
+    };
+    const poll = () => {
+      if (!isTabActive || !isOnline) return;
+      subscription.add(
+        getObservable().subscribe({
+          next: sub.next.bind(sub),
+          error: sub.error.bind(sub),
+        })
+      );
+      lastPoll = new Date();
+    };
+
+    subscription.add(
+      fromEvent(document, 'visibilitychange').subscribe(handleVisibleChange)
+    );
+    subscription.add(fromEvent(window, 'online').subscribe(handleOnline));
+    subscription.add(fromEvent(window, 'offline').subscribe(handleOffline));
+    timerSubscription = timer(0, POLL_INTERVAL_MS).subscribe(poll);
+
+    return () => {
+      subscription.unsubscribe();
+      timerSubscription?.unsubscribe();
+    };
   }
 }
